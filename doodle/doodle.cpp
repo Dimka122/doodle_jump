821b15d301e2f66825e8933a67eb389e0b3aec4a
Дмитрий Кассин <kassindima1@gmail.com>
2021-10-06 11:21:00 +0300

    newзаново

--- doodle/doodle.cpp
+++ doodle/doodle.cpp
@@ -1,18 +1,12 @@
 #include<SFML/Graphics.hpp>
-#include <iostream>
-#include"MainWindow.h"
 
 using namespace sf;
 
-
-
-
-
 int main()
 {
-	//RenderWindow window(VideoMode(400, 400), "SFML works");
+	RenderWindow window(VideoMode(400, 400), "SFML works");
 
-	/*while (window.isOpen())
+	while (window.isOpen())
 	{
 		Event event;
 		while (window.pollEvent(event))
@@ -21,20 +15,8 @@ int main()
 
 				window.close();
 		}
-		
+
 		window.display();
 	}
-
-
-	return 0;*/ 
-	MainWindow * window = new MainWindow(532, 850);
-	window->setBgColor(127, 176, 48);
-	//window->setBgColor(sf::Color::Green);
-	window->show();
-	window->show();
-	window->show();
-
-
-	delete window;
 	return 0;
 }
